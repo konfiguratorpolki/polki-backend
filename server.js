0f941a42fd0df2402036eaa9d53fa62e4d3dbdc1
konfiguratorpolki <nowywymiar2@gmail.com>
2026-03-07 12:56:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ try {
     const nodemailer = require('nodemailer');
     mailer = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587, secure: false,
+        port: 465, secure: true,
         auth: { 
             user: process.env.SMTP_USER, 
             pass: process.env.SMTP_PASS
